1549d232c0794e2461fc641aa22d3e290f19eaf2
Andersson <anderssonbustos@gmail.com>
2023-06-21 22:59:16 -0500

    refactorizacion de diseño

---
+++ Proyecto-react/src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles.css';
+import logo from './assets/menu.png';
+import logo1 from './assets/reanudar.png';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='navbar'>
+                <a href="#" className='Logo'><figure><img src={logo1} className="logoNav" /></figure></a>
+                <input type="checkbox" id="toggler" />
+                <label htmlFor="toggler"><figure><img src={logo} alt="" /></figure></label>
+                <div className='menu'>
+                    <ul className='list'>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Skills</a></li>
+                        <li><a href="#">Estudios</a></li>
+                        <li><a href="#">Experiencia Laboral</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
